d2576792350267c77e318602855cbd18957980b4
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2023-04-27 11:40:30 +0530

    change j

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:yitaku/screen/Auth/BlueLogin/blueLoginScreen.dart';
-import 'package:yitaku/screen/Auth/createAccount/create_account_screen.dart';
-import 'package:yitaku/screen/complete_signup/complete_signin_screen.dart';
-import 'package:yitaku/screen/dashbord/dashboard_screen.dart';
-import 'package:yitaku/screen/homeScreen/home_screen.dart';
-import 'package:yitaku/screen/home_screen_two/home_screen_two.dart';
-import 'package:yitaku/screen/intro_screen/intro_screen.dart';
-import 'package:yitaku/screen/loginpage_new/login_new.dart';
-import 'package:yitaku/screen/splash_screen/splash_screen.dart';
-import 'package:yitaku/screen/typeSeller_screen/type_seller_screen.dart';
 
-import 'screen/goal_screen/goal_screen.dart';
+
+import 'package:yitaku/screen/splash_screen/splash_screen.dart';
+
 
 
 void main() {
